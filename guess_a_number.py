59b535e582faa4927a38f8117d9b084351d7b0b4
Shaver <sshave4545@greenville.k12.sc.us>
2017-09-14 14:25:33 -0400

    added limit

--- guess_a_number.py
+++ guess_a_number.py
@@ -1,12 +1,20 @@
+
+#config
+
 import random
 low = 1
 high = 100
+limit = 10
+
 rand = random.randint(low,high)
 print("I'm thinking of a number from " + str(low) + " to " + str(high)+ ".");
 
+# start game 
 guess = -1
+tries=0
 
-while guess != rand:
+# play game 
+while guess != rand and tries < limit:
     guess = input("Take a guess: ")
     guess = int(guess)
     
@@ -17,4 +25,10 @@ while guess != rand:
     else:
         print("You got it!")
 
-print("Game over")
+    tries += 1
+
+#game end
+if guess == rand:
+    print( "Way to go. You can guess!")
+else:
+    print( " Way to go, you suck the number was " + str(rand) + ".")
