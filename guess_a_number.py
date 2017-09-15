058e16e1cbd643fc8dd5ce88d48ae52c8ecaedf2
Shaver <sshave4545@greenville.k12.sc.us>
2017-09-15 14:25:56 -0400

    added loop so only numbers can be entered

--- guess_a_number.py
+++ guess_a_number.py
@@ -7,16 +7,29 @@ high = 100
 limit = 10
 
 rand = random.randint(low,high)
-print("I'm thinking of a number from " + str(low) + " to " + str(high)+ ".");
+print("I'm thinking of a number from " + str(low) + "  to " + str(high)+ ".");
 
-# start game 
+#helper fuctions
+def get_guess():
+    while True:
+        guess = input ("Guess a number: ")
+
+        if guess.isnumeric():
+            guess = int(guess)
+            return guess
+        else:
+            print("You must enter a number.")
+        
+# start game
+rand = random.randint(low,high)
+print("Im thinking of a number from" +str(low) + " to " + str(high) +". You have" + str(limit) + "tries to guess it.")
+
 guess = -1
 tries=0
 
 # play game 
 while guess != rand and tries < limit:
-    guess = input("Take a guess: ")
-    guess = int(guess)
+    guess = get_guess()
     
     if guess < rand:
         print("You guessed too low.")
