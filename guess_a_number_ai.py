a60928299513daf85589fd91f614eb28abe6c252
artsyone <31486667+artsyone@users.noreply.github.com>
2017-10-06 14:41:42 -0400

    Add files via upload

--- guess_a_number_ai.py
+++ guess_a_number_ai.py
@@ -8,10 +8,7 @@ import random
 
 # config
 import math
-
-low = 1
-high = 1000
-limit = math.ceil (math.log( high - low + 1 ,2))
+tries = 1 
 
 # helper functions
 
@@ -26,6 +23,7 @@ def show_start_screen():
 / /_/ / /_/ /  __(__  |__  )  / ___ |   / /|  / /_/ / / / / / / /_/ /  __/ /    
 \____/\__,_/\___/____/____/  /_/  |_|  /_/ |_/\__,_/_/ /_/ /_/_.___/\___/_/     
                                                                                 """)
+    print("")
   
 
 def show_credits():
@@ -37,25 +35,52 @@ def show_credits():
 
     
     print("This awesome game was created by Savanna S.")
+    print(" That was fun! Bye " + str(name) + "." )
+    
+
+def ask_name():
+    
+    print ("Before we start I would like to know who im playing against ")
+    print("Type your name")
+    name = input ()
+
+    return name
+
+def pick_low():
+        print("Please enter the numbers we will play between " + str(name) + ".")
+        print(" Enter the low ")
+        low = input()
+        
+
+        return int(low) 
+       
+def pick_high ():
+        print ("Enter the high ")
+        high = input ()
+
+        return int(high)
     
 def get_guess(current_low, current_high):
     """
     Returns a truncated average of the current low and high
     """
     guess = (current_high + current_low )//2
+    
    
     return guess 
 
-def pick_number():
-    """
-    Ask the player to pick a number and waits until the player
+def pick_number(current_low,current_high,name):
+    """    Ask the player to pick a number and waits until the player
     confirms they have a number by pressing enter.
     """
-   
-    print( " Think of a number from " +  str(low) + " to " + str(high) + " . Im going to try to guess it. " )
+    
+    
+    print( " Think of a number from " +  str(current_low) + " to " + str(current_high) + " . Im going to try to guess it." )
+    print("")
+    print(" I will get " + str(limit) + " tries, to guess your number. ")
     print ("   ")
     time.sleep(1)
-    print("Press enter to contine")
+    print("Press enter to contine " + str(name) + ".")
     print ("   ")
     input ()
 
@@ -74,72 +99,81 @@ def check_guess(guess):
              1 if the guess was too high
     """
     
-   
     while True:
-        print (" Is the number " + str(guess) + "." )
+        print("")
+        print (" Is your number " + str(guess) + "." )
         print ("   ")
         time.sleep(1) 
 
-        decision = input(" Is the number your thinking of... Enter (lower / higher / yes)")
+        print(" Guess " + str(tries) + " of " + str(limit)+ ".")
+        decision = input(" Please input whether the number your thinking of is ...(lower / higher / yes)")
         print("")
+
         
-        if decision == 'low' or decision == 'lower'or decision == 'lower' or decision == 'l':
+        decision = decision.lower()
+        if decision == 'low' or decision == 'lower'or decision == 'l':
             return 1
         elif decision == 'high' or decision == 'higher'or decision == 'h':
             return -1
         elif decision == 'yes'or decision == 'yes'or decision == 'yeah' or decision == 'y':
-            print("Haha I got it.")
-            print("")
             return 0
 
-            
-    
+        else:
+            print(" **** I dont understand " + str(name)+ " please print h,l or y. ***** ")
 
 def show_result():
-    pass
+    print("Haha I got it.")
+    print("")
 
 def play_again():
     while True:
         decision = input("Would you like to play again? (y/n) ")
         
-
         if decision == 'y' or decision == 'yes':
             return True
         elif decision == 'n' or decision == 'no':
             return False
         else:
-            print("I don't understand. Please enter 'y' or 'n'.")
+            print("I don't understand " + str(name) + " . Please enter 'y' or 'n'.")
 
-def play():
-    current_low = low
-    current_high = high
+def play(name):
+
+    current_low = pick_low()
+    current_high = pick_high()
+    
+    global tries 
+    tries = 1 
+       
     result = -1
 
- 
+    global limit 
+    limit = math.ceil(math.log( current_high - current_low + 1 ,2))
     
-    pick_number()
-    
+    pick_number (current_low,current_high,name)
+
     while result != 0:
         guess = get_guess(current_low, current_high)
-
         result = check_guess(guess)
-
+    
+            
         if result == -1 :         
             current_low = guess + 1 
-            
+            tries += 1 
         elif result == 1 :
-             current_high = guess - 1  
-            
-
+            current_high = guess - 1
+            tries += 1
+    
     show_result()
 
 # Game starts running here
 show_start_screen()
-
+name = ask_name()
 playing = True
 
 while playing:
-    play()
+    play(name)
+    
     playing = play_again()
+    
 
 show_credits()
