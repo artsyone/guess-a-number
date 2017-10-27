774930bf5aa0aa28e32a0144f2d12ff4ca6e425c
artsyone <31486667+artsyone@users.noreply.github.com>
2017-10-27 14:50:19 -0400

    Add files via upload

--- final2.py
+++ final2.py
@@ -1,8 +1,21 @@
 from turtle import *
 
+window_height()
+400
+window_width ()
+640
+
+
 speed(0)
 shape("turtle")
 pensize(1)
+
+
+
+
+
+    
+
 
 
 def draw_circle(d, c, p):
@@ -14,11 +27,14 @@ def draw_circle(d, c, p):
     ht()
     speed(0)
     penup()
-    setpos(0,-200)
+    setpos(0,-300)
+    penup()
     pendown()
     
     count = 0
 
+
+    
 def draw_all_circles(c1,c2,c3,c4,c5,c6,c7,c8):
      
     for i in range (10,100,10):
@@ -31,21 +47,44 @@ def draw_all_circles(c1,c2,c3,c4,c5,c6,c7,c8):
     for i in range (300,400,10):
         draw_circle(i,c4,3)
     
-    for i in range (500,600,10):
+    for i in range (400,500,10):
         draw_circle(i,c5,6)
 
-    for i in range (700,800,10):
+    for i in range (600,700,10):
         draw_circle(i,c6,6)
 
-    for i in range (900,1000,10):
+    for i in range (800,900,10):
         draw_circle(i,c7,6)
 
-    for i in range (1000,1100,10):
+    for i in range (900,1000,10):
         draw_circle(i,c8,9)
 
     
         
     
-draw_all_circles("red","orange","yellow","green","blue","BlueViolet","viloet","black")
-draw_all_circles("orange","yellow","green","blue","BlueViolet","violet","black","red")
+draw_all_circles("red","orange","yellow","green","blue","BlueViolet","violet","black")
+
+
+draw_all_circles("black","black","black","black","black","black","black","black")
+
+draw_all_circles("white","white","white","white","white","white","white","white")
+
+
+
+    
+def k1():
+    bgcolor("white")
+
+def k2():
+    bgcolor("black")
+    
+
+onkey(k1, "Up")
+onkey(k2, "space")
+
+    
+
+listen()
+mainloop()
+    
 
